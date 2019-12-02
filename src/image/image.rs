ceb10d60f91a31a11e343f691045a75ed86e0ba9
elisvathi <elisvathi@outlook.com>
2019-12-02 23:48:47 +0100

    Added image builder

--- src/image/image.rs
+++ src/image/image.rs
@@ -6,6 +6,51 @@ pub struct Image {
     pub width: usize,
     pub height: usize,
 }
+pub struct ImageBuilder {
+    pub pixels: Vec<Vec<Vec3>>,
+    pub passes: Vec<Vec<usize>>,
+    pub max_passes: usize,
+    pub finished: usize,
+    pub width: usize,
+    pub height: usize,
+}
+impl ImageBuilder {
+    pub fn new(width: usize, height: usize, max_passes: usize) -> ImageBuilder {
+        let data = vec![vec![Vec3::zero(); width]; height];
+        let passes = vec![vec![0; width]; height];
+        ImageBuilder {
+            pixels: data,
+            width,
+            height,
+            finished: 0,
+            passes,
+            max_passes,
+        }
+    }
+
+    pub fn to_image(&self) -> Image {
+        let mut image = Image::new(self.width, self.height);
+        for i in 0..self.width {
+            for j in 0..self.height {
+                image.pixels[j][i] = &self.pixels[j][i] / self.passes[j][i] as Float;
+            }
+        }
+        image
+    }
+
+    pub fn is_finished(&self) -> bool {
+        self.finished >= self.max_passes
+    }
+
+    pub fn put_pixel(&mut self, pixel: Vec3, j: usize, i: usize) {
+        self.pixels[j][i] = &self.pixels[j][i] + &pixel;
+        self.passes[j][i] += 1;
+        if self.passes[j][i] == self.max_passes {
+            self.finished += 1;
+        }
+    }
+}
+
 impl Image {
     pub fn new(width: usize, height: usize) -> Image {
         let data = vec![vec![Vec3::zero(); width]; height];
@@ -15,8 +60,9 @@ impl Image {
             height,
         }
     }
-    pub fn put_pixel(&mut self, color: Vec3, x: usize, y: usize) {
-        self.pixels[x][y] = color;
+
+    pub fn put_pixel(&mut self, color: Vec3, y: usize, x: usize) {
+        self.pixels[y][x] = color;
     }
 
     pub fn get_pixel(&self, x: usize, y: usize) -> Vec3 {
